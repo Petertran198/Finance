9b20402382109f5e8c71b140264bae11365dc2d5
petertran198 <petertran198@gmail.com>
2019-01-19 14:58:54 -0500

    Implemented many to many relationship for stocks and users table through the user_stocks table

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,13 +1,20 @@
 class Stock < ApplicationRecord
-  
+  has_many :user_stocks
+  has_many :user, through: :user_stocks
   def self.new_from_lookup(ticker_symbol)
     begin
         looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
         Stock.new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
     rescue Exception => e
-      return nil    
+      return nil 
     end
   end  
+
+  ##
+
+  def self.strip_commas(number)
+    number.to_s.gsub(",", "")
+  end
 
 
 end
